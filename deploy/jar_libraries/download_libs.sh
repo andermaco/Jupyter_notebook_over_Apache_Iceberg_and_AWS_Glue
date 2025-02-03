6d8cb00e01f4fa14850175881dbc29ebf713c80c
andermaco@gmail.com <andermaco@gmail.com>
2025-02-03 19:46:43 +0100

    update readme and libs

---
+++ deploy/jar_libraries/download_libs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="."
+
+URLS=(
+  "https://repo1.maven.org/maven2/software/amazon/awssdk/bundle/2.17.161/bundle-2.17.161.jar"
+  "https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-spark-runtime-3.1_2.12/0.13.1/iceberg-spark-runtime-3.1_2.12-0.13.1.jar"
+  "https://repo1.maven.org/maven2/software/amazon/awssdk/url-connection-client/2.17.161/url-connection-client-2.17.161.jar"
+)
+
+for URL in "${URLS[@]}"; do
+  FILENAME=$(basename "$URL")  
+  wget -q "$URL" -O "$DIR/$FILENAME"
+  echo "Download: $FILENAME"
+done
