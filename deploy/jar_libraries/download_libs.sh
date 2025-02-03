ce3da7b5206ad5200bc72fbf05f4179848d16db2
andermaco@gmail.com <andermaco@gmail.com>
2025-02-03 19:59:40 +0100

    update shell script

--- deploy/jar_libraries/download_libs.sh
+++ deploy/jar_libraries/download_libs.sh
@@ -4,7 +4,7 @@ DIR="."
 
 URLS=(
   "https://repo1.maven.org/maven2/software/amazon/awssdk/bundle/2.17.161/bundle-2.17.161.jar"
-  "https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-spark-runtime-3.1_2.12/0.13.1/iceberg-spark-runtime-3.1_2.12-0.13.1.jar"
+  "https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-spark-runtime-3.3_2.12/1.6.1/iceberg-spark-runtime-3.3_2.12-1.6.1.jar"
   "https://repo1.maven.org/maven2/software/amazon/awssdk/url-connection-client/2.17.161/url-connection-client-2.17.161.jar"
 )
 
